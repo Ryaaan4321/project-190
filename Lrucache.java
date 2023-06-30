4a591c32fa3b22ccbd7f80a5808ef14ee4fecab3
aryan <91269171+Ryaaan4321@users.noreply.github.com>
2023-06-30 16:26:51 +0530

    Lrucache.java

--- Lrucache.java
+++ Lrucache.java
@@ -62,6 +62,72 @@ public class Lrucache {
         }
     }
 
+
+
+    //// all the test cases are approved in this code snipppet..!
+import java.util.HashMap;
+
+public class LRUCache {
+    public static class ListNode {
+        ListNode prev, next;
+        int key, value;
+
+        ListNode(int _key, int _value) {
+            key = _key;
+            value = _value;
+        }
+    }
+
+    ListNode head = new ListNode(0, 0);
+    ListNode tail = new ListNode(0, 0);
+    int capacity;
+    HashMap<Integer, ListNode> hashMap;
+
+    public LRUCache(int _capacity) {
+        capacity = _capacity;
+        hashMap = new HashMap<>();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        if (hashMap.containsKey(key)) {
+            ListNode node = hashMap.get(key);
+            remove(node);
+            insert(node);
+            return node.value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (hashMap.containsKey(key)) {
+            remove(hashMap.get(key));
+        }
+        if (hashMap.size() == capacity) {
+            remove(tail.prev);
+        }
+        insert(new ListNode(key, value));
+    }
+
+    private void remove(ListNode node) {
+        hashMap.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        if (tail.prev == node) {
+            tail.prev = node.prev;
+        }
+    }
+
+    private void insert(ListNode node) {
+        hashMap.put(node.key, node);
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+}
+
     public static void main(String[] args) {
         LRUCache cache = new LRUCache(3); // Create a cache with capacity 2
 
